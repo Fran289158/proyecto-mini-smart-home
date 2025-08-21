f4df79e5a38a5ee38413a42129243c4621a5e0b6
Francisco-Scarinci <50156336@est.ort.edu.ar>
2025-08-21 09:06:14 -0300

    Subo Progreso

---
+++ back end/datos.js
@@ -0,0 +1,11 @@
+function CrearCuenta (FechaNacimiento, Usuario, Contraseña, Genero) {
+    let lista = ["Fran", "Roberto", "Wally", "Carlos"]
+    for (let i = 0; i <= lista.length; i++) {
+        let elemento = lista [i];
+        console.log (elemento);
+    }
+}
+
+function LOGIN (Usuario, Contraseña) {
+
+}
