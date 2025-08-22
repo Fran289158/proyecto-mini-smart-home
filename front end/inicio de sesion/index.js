ea4da9bd7e74ded9b405e47f30f8f3a1aa287881
Mica <you@example.com>
2025-08-22 15:47:26 -0300

    empece con el js, ni cerca de terminado

---
+++ front end/inicio de sesion/index.js
@@ -0,0 +1,7 @@
+let usuario = document.getElementById("dondevaelmail").value;
+let contraseña = document.getElementById("dondevalaclave").value;
+let boton = document.getElementById("iniciar");
+
+function iniciosesion() {}
+
+document.addEventListener("click",iniciosesion);
