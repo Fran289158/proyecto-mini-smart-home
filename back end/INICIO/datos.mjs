d71880238e2f51d88495844b0d4466f1c7c4484b
Francisco-Scarinci <50156336@est.ort.edu.ar>
2025-09-01 10:36:03 -0300

    Subo progreso

--- back end/INICIO/datos.mjs
+++ back end/INICIO/datos.mjs
@@ -2,5 +2,12 @@
 import fs from "fs";
 
 function LOGIN (Dia, Mes, Año, NombreUsuario, Contraseña, Genero) {
-   let leer = fs.readFileSync ("clientes.json", "utf-8"); 
+
+   let leer = fs.readFileSync ("clientes.json", "utf-8");
+
+   let conversion = JSON.parse (leer);
+
+   for (let i = 0; i < conversion.length; i++) {
+      
+   }
 }
