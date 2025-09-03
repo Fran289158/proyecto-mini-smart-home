b6d61694a3be0fdeacaac12ea0b67146f4933933
Francisco-Scarinci <50156336@est.ort.edu.ar>
2025-09-03 15:53:42 -0300

    Subo progreso

--- back end/INICIO/datos.mjs
+++ back end/INICIO/datos.mjs
@@ -1,15 +1,12 @@
 //Importo librería File System:
 import fs from "fs";
-
+//
+//
 function Registro () {
-
-   let leer = fs.readFileSync ("clientes.json", "utf-8");
-
-   let conversion = JSON.parse (leer);
-
-   for (let i = 0; i < conversion.length; i++) {
-      let NuevoNombre = conversion.push ("Francisco");
-      console.log (NuevoNombre);
-   }
+   let usuario = "Francisco";
+   let contraseña = "Fran293739";
+   return (usuario + "\n" + contraseña);
 }
-Registro ();
+
+subscribeGETEvent ()
+
