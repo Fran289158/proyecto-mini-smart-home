4bc4100af4d23c003591ade61f662f69f7bafeeb
Mica <you@example.com>
2025-09-03 15:41:18 -0300

    IVO. IVO NO LO PUEDO CREER. HICE ALGO. MAS O MENOS FUNCIONA. VEO TODO CON MAS COLOR. ME VOY A VOLVER RELIGIOSA. HALELUYAAAAA

--- front end/registro/index.js
+++ front end/registro/index.js
@@ -1,9 +1,33 @@
 let usuario = document.getElementById("dondevaelmail");
 let contraseña = document.getElementById("dondevalacontra");
 let boton = document.getElementById("registrate");
+let usuarios=[] 
+let contraseñas=[]
 
 function registrar(){
-  if(usuario===""){
-    alert("Por favor, ingresa tu mail o  nombre de usuario.");
+
+  const mail = usuario.value;
+  const contra = contraseña.value;
+
+  if(mail==="" || contra === ""){
+    mensaje.textContent = "Completar ambos campos";
+    mensaje.style.color = "red";
+    return;
+  }
+
+  if(usuarios.includes(mail)){
+    mensaje.textContent = "Este usuario ya existe";
+    mensaje.style.color = "red";
     return
   }
+  else{ 
+    usuarios.push(mail);
+    contraseñas.push(contra);
+    mensaje.textContent = ""
+    
+  console.log("Usuarios:", usuarios);
+  console.log("Contraseñas:", contraseñas);
+  }
+}
+
+boton.addEventListener("click",registrar);
