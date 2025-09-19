e6da0916950c7d9b81f04de0d16d452d4e98ff0a
Francisco <50156336@est.ort.edu.ar>
2025-09-19 15:51:56 -0300

    Subo progreso

--- back end/INICIO/index.js
+++ back end/INICIO/index.js
@@ -1,17 +1,8 @@
-//Importo librería File System:
 import fs from "fs";
-import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from 'soquetic';
+import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 
-subscribePOSTEvent ("login", (data) => {
-  
-  try { 
-  let Data1 = JSON.parse (fs.readFileSync ("main.json", "utf-8"));
-  return (data1);
-  }
-  catch (error) {
-    console.error ("Error:", error);
-  }
-
+subscribePOSTEvent ("mensaje", (data) => {
+  let leerArchivoFRONT = JSON.parse (fs.readFileSync ("front end/inicio de sesion/index.js"));
+  data.find ()
 })
-
 startServer ();
