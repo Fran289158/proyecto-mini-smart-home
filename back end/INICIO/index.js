0b1019440aeb9450c2b1604d9176572009ba5574
Francisco <50156336@est.ort.edu.ar>
2025-09-20 15:58:31 -0300

    Subo progreso

--- back end/INICIO/index.js
+++ back end/INICIO/index.js
@@ -1,13 +1,4 @@
 import fs from "fs";
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 
-function REGISTRO () {
-  let data = JSON.parse (fs.readFileSync ("main.json", "utf-8"));
-  let objeto = {
-    email: "scarincifrancisco@gmail.com",
-    password: "Pepito10"
-  }
-  data.push (objeto);
 
-  fs.writeFileSync ("main.json")
-}
