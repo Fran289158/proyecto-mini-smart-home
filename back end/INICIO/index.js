439bb217a3423481f813f7fa573d1dedaec89a92
Francisco <50156336@est.ort.edu.ar>
2025-09-20 13:58:55 -0300

    Subo progreso

--- back end/INICIO/index.js
+++ back end/INICIO/index.js
@@ -1,8 +1,13 @@
 import fs from "fs";
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 
-subscribePOSTEvent ("mensaje", (data) => {
-  let leerArchivoFRONT = JSON.parse (fs.readFileSync ("front end/inicio de sesion/index.js"));
-  data.find ()
-})
-startServer ();
+function REGISTRO () {
+  let data = JSON.parse (fs.readFileSync ("main.json", "utf-8"));
+  let objeto = {
+    email: "scarincifrancisco@gmail.com",
+    password: "Pepito10"
+  }
+  data.push (objeto);
+
+  fs.writeFileSync ("main.json")
+}
