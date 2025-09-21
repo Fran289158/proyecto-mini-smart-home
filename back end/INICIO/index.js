8ebb86a7e75d72e3ded8207f356e7671d36e846a
Francisco <50156336@est.ort.edu.ar>
2025-09-21 10:11:43 -0300

    SIIIIIII LA PUTA MADRE QUE LO RE MIL PARIO LO ENTENDI TOMAAAA SOQUETIC GORDA PUTA

--- back end/INICIO/index.js
+++ back end/INICIO/index.js
@@ -1,8 +1,9 @@
 import fs from "fs";
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 
-subscribePOSTEvent ("mensaje", (data) => {
-  let leerArchivoFRONT = JSON.parse (fs.readFileSync ("front end/inicio de sesion/index.js"));
-  data.find ()
+subscribePOSTEvent ("login", (data) => {
+  return (`Mensaje recibido: ${data.msg}`);
+
 })
+
 startServer ();
