75aadf973ca841aaaac976ea7b344f97d4dbda13
Francisco <50156336@est.ort.edu.ar>
2025-09-24 11:48:44 -0300

    DALE COMMITEATE

--- back end/INICIO/index.js
+++ back end/INICIO/index.js
@@ -1,8 +1,24 @@
 import fs from "fs";
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 
-subscribePOSTEvent ("mensaje", (data) => {
-  let leerArchivoFRONT = JSON.parse (fs.readFileSync ("front end/inicio de sesion/index.js"));
-  data.find ()
+subscribePOSTEvent ("register", (data) => {
+  let leer = JSON.parse (fs.readFileSync ("login + registro.json", "utf-8"));
+  let objeto = {email: data.email, password: data.password};
+  leer.push (objeto);
+
+  fs.writeFileSync ("login + registro.json", (leer, null, "\n"), {encoding: "utf-8"});
 })
+
+subscribePOSTEvent ("login", (data) => {
+  let leer = JSON.parse (fs.readFileSync ("login + registro.json", "utf-8"));
+  let encontrar = leer.find (leer => data.email === leer.email & data.password === leer.password);
+
+  if (encontrar) {
+    return {ok: true};
+  }
+  else {
+    return {ok: false};
+  }
+})
+
 startServer ();
